01301ebde0234427b19448f9c5df8c127751203d
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-05-03 10:11:17 +0300

    Create ARCLABHW1Q2.asm

---
+++ ARCLABHW1Q2.asm
@@ -0,0 +1,8 @@
+#Eitan Brown 346816549
+#Done with Yoni Rubin
+
+#Write a program that gets a decimal number (double digits) through the keyboard and finds the sum of all the numbers received so far. 
+#When the value 0 is entered, the program stops and prints the sum on the screen. 
+#Note: You must verify that the number is between -99 and +99. For the purposes of this program, consider single digit numbers as the same as two digit numbers.
+
+

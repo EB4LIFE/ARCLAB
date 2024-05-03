9d3d4763f5be8df7c9143b5b81021e72a571f61b
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-05-03 10:07:09 +0300

    Update and rename ARCLABHW1.asm to ARCLABHW1q1.asm

--- ARCLABHW1q1.asm
+++ ARCLABHW1q1.asm
@@ -1,6 +1,6 @@
-Eitan Brown
-346816549
-Done with Yoni Rubin
+#Eitan Brown
+#346816549
+#Done with Yoni Rubin
 
 #Write a program that copies a block of words that starts at an address located in $a0 to a block of words that begin with an address found in $a1. 
 #The program will count the number of words copied and save the result in the register $v0. 
@@ -16,12 +16,14 @@ la $a0 Array 1
 la $a1 Array 2
 
 Loopstart:
-lw $t0 ($a0)
-sw $t0 ($a1)
+lw $t0 ($a0) #copied address into $t0 
+sw $t0 ($a1) #saving it "pasting" int0 $a1
 
-beq $t0 $zero exit
+beq $t0 $zero exit 
+#line 22 if copied value is zero , it will be the last one copied but not counted so we exit here.
 
-addi $s0 $s0 1
+addi $v0 $v0 1 
+#Line 24 counts the number of words copied over (increments one at a time)
 
 addi $a0 $a0 4
 addi $a1 $a1 4

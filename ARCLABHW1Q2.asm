576b4e5606dc32b3efc2633d30bab41c956c2270
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-05-05 03:41:10 -0400

    Update ARCLABHW1Q2.asm

--- ARCLABHW1Q2.asm
+++ ARCLABHW1Q2.asm
@@ -5,5 +5,65 @@
 #When the value 0 is entered, the program stops and prints the sum on the screen. 
 #Note: You must verify that the number is between -99 and +99. For the purposes of this program, consider single digit numbers as the same as two digit numbers.
 
+.data
 
+prompt: asciiz "Enter a number between-99 and 99:\n"
+error: asciiz "Number must be between-99 and 991\n"
+result: .asciiz "Sum of numbers is
+
+
+.text
+
+loop:
+#Prompt for user input
+la $a0 prompt
+addi $vo $zero 4
+syscall
+
+#Read user input
+addi $v0 $zero 5
+syscall
+
+
+#If input is 0, exit loop
+beq $v0 $zero exit
+#Check if input is between -99 and 99
+slti $to $v0 -99
+slti $t1 $v0 100
+
+xor $to $to $t1
+
+#If within range, continue
+bne $to $zero continue
+#If out of range, print error and input number again
+la $a0 errог
+
+addi $vo Szero 4
+syscall
+
+j loop
+
+continue:
+
+#Add input to sum
+
+add $50 $50 $v0
+
+j loop
+
+exit:
+
+#Print sum of all inputs
+
+la $a0 result
+
+addi $vo $zero 4
+
+syscall
+
+move $a0 $50
+
+addi $v0 $zero 1
+
+syscall
 

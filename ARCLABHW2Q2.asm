24234a74e41f38556e6f5454a3656eadddc97534
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-05-11 15:46:59 -0400

    Update ARCLABHW2Q2.asm

--- ARCLABHW2Q2.asm
+++ ARCLABHW2Q2.asm
@@ -1,1 +1,14 @@
+#Eitan Brown 
+#346816549
+#Done with Yoni Rubin
 
+
+
+#Write a program for the implementation of a simple calculator according to the following rules: 
+#A) The computer will display the following message: ENTER VALUE. In response the user enters a decimal value. The data will be input as an INTEGER. 
+#B) The computer displays a message on the screen: ENTER OP-CODE . 
+#The input will be in the CHAR format and is one of 4 options: *, - , + , @ (multiply, subtract, addition, end the program, respectively). 
+#C) If the user ends the program, the result of the calculation should be displayed with the accompanying message : The result is 
+#D) If a non-termination action code is requested, then: The computer will display the following message: ENTER VALUE In response, 
+#the user will enter a decimal value. The data will be input as INTEGER. 
+#Assume that the maximum addition and subtraction operations are 32 bit. 

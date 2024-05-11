dc061e7442307d4bdebbbc8239357fdc426051b4
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-05-11 15:45:09 -0400

    Update ARCLABHW2Q1.asm

--- ARCLABHW2Q1.asm
+++ ARCLABHW2Q1.asm
@@ -1,1 +1,11 @@
+#Eitan Brown
+#346816549
+#Done with Yoni Rubin
+
+#Topic: Finding the biggest number.
+#A data list is given in the data memory. Each data is sized in bytes. The list begins at the address located in the register $a0 and its length is given in register $a1. Assume that the data list is signed. 
+#Find the largest number and enter it into register $v0. Display the largest data on your computer screen. 
+#Remarks: 
+#Each program should be accompanied by a flowchart (and the algorithm of the solution if necessary). 
+#Comments in the body of the program are mandatory. 
 

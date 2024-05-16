9f33f21a94fcc4028c5c79b6ee5f60c9e3f10a2c
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-05-16 14:14:29 +0300

    Create ARCLABHW3Q1.asm

---
+++ ARCLABHW3Q1.asm
@@ -0,0 +1,14 @@
+#Eitan Brown
+#346816549
+#Done with Yoni Rubin
+
+#Subject: Bubble sorting
+#Store a numeric list of data in the data memory. The size of each datum is one word. The
+#address of the list should be stored in $a0 and its length in $a1.
+#Allow for the data list to be signed.
+#Write a program that sorts the list of data by bubble sort. Then print the now sorted list to the
+#screen.
+#Reminder:
+#Bubble Sort is an algorithm that scans the data list and compares each pair of data. If the value
+#given on the left is greater than the value of the right, the algorithm will switch them. In each
+#scan of the array the largest number "bubbles" to the top of the list.

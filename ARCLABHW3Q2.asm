8d4896df3ab69d6479035f93b2120d425aa0bce4
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-05-16 14:16:06 +0300

    Create ARCLABHW3Q2.asm

---
+++ ARCLABHW3Q2.asm
@@ -0,0 +1,14 @@
+#Eitan Brown
+#346816549
+#Done with Yoni Rubin
+
+#Exercise 2 Detecting Progressions
+#Create a numeric list of data where each data is one byte. The address of the list should be
+#stored in $a0 and its length should be stored in register $a1.
+#Allow for the data list to be signed.
+#Write a program that scans the data list and checks whether the data is an arithmetic
+#progression or a geometric progression or both or neither (i.e. the list does not constitute a progression at all).
+#The program should also print a message about the scan conclusion.
+#It should print the initial value of the progression and for an arithmetic progression it should print
+#“d = “ followed by the difference between each element. If the list is a geometric progression, it should print “q = “ followed by the quotient.
+#If the list does not constitute a progression at all, print “Not a progression”.
